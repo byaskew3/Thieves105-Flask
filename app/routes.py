8c1e8968b6d53f3c98c00b048a384d02689c9c75
byaskew3 <byaskew3@gmail.com>
2022-11-28 18:50:09 -0700

    day1 completed

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/home')
+def home():
+    instructors = [{
+        'name': 'Christian',
+        'age': 9000
+    },
+    {
+        'name': 'Christopher',
+        'age': 'infinity'
+    }]
+    return render_template('index.html', names=instructors)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
